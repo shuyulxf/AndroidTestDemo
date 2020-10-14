f7a007cb4f0cea0ba58c6f050125217bfbaffa97
李习凤 <lixifeng@bytedance.com>
2020-10-14 20:49:52 +0800

    feat: init

--- app/src/main/java/com/example/androidtestdemo/main/MainActivity.kt
+++ app/src/main/java/com/example/androidtestdemo/main/MainActivity.kt
@@ -9,11 +9,7 @@ import com.example.androidtestdemo.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Hide navigation bar and status bar.
-        window.decorView.apply{
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
+
         setContentView(R.layout.main_activity)
     }
 }
